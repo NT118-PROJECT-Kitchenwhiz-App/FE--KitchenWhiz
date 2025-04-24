5439b2ccbe999d04dc9a19500e518848bce4bf7b
Uyên Thy <uynthyee.313@gmail.com>
2025-04-24 15:18:05 +0700

    add intent for login activity

--- app/src/main/java/com/example/kitchenwhiz/MainActivity.java
+++ app/src/main/java/com/example/kitchenwhiz/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.kitchenwhiz;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +15,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+Button btnlogin;
+EditText tbname, tbpass;
+TextView txtforgotpass, txtregister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +27,42 @@ public class MainActivity extends AppCompatActivity {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            mapping();
+            btnlogin.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (tbname.getText() == null || tbpass.getText() == null){
+                        Toast.makeText(MainActivity.this, "Vui lòng nhập đầy đủ thông tin!", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+
+                }
+            });
+
+            txtforgotpass.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this, forgot_password.class);
+                    startActivity(intent);
+                }
+            });
+
+            txtregister.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this, signup.class);
+                    startActivity(intent);
+                }
+            });
             return insets;
         });
+    }
+
+    private void mapping(){
+        btnlogin = findViewById(R.id.login_button);
+        tbname = findViewById(R.id.email_username);
+        tbpass = findViewById(R.id.loginpassword);
+        txtforgotpass = findViewById(R.id.forgot_password);
+        txtregister = findViewById(R.id.sign_up);
     }
 }
