20ddc56b9e1513ca1e712c1dd0ab0ea16e58329b
Uyên Thy <uynthyee.313@gmail.com>
2025-05-18 15:40:19 +0700

    get image from folder

--- app/src/main/java/com/example/kitchenwhiz/Activity/Add_food.java
+++ app/src/main/java/com/example/kitchenwhiz/Activity/Add_food.java
@@ -1,7 +1,11 @@
 package com.example.kitchenwhiz.Activity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -9,9 +13,11 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -19,22 +25,23 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.kitchenwhiz.R;
 
+import java.io.File;
+import java.io.FileOutputStream;
+
 public class Add_food extends AppCompatActivity {
     ImageView image;
     EditText txtname, txtdes, txtser, txttime,txtins;
     Button btnadd;
     TextView btnaddin;
     LinearLayout ingredientContainer;
-    ActivityResultLauncher<Intent> pickImageLauncher;
+    ActivityResultLauncher<Intent> pickImage;
+    File imageFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_add_food);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             mapping();
             addRow();
             addRow();
@@ -44,6 +51,28 @@ public class Add_food extends AppCompatActivity {
                     addRow();
                 }
             });
+
+            pickImage = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+                if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+                    Uri imageUri = result.getData().getData();
+                    try {
+                        Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
+                        image.setImageBitmap(bitmap);
+                        imageFile = saveBitmapToFile(bitmap);
+                    } catch (Exception ex) {
+                        Toast.makeText(this, "Error loading image: " + ex.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+            image.setOnClickListener(v -> {
+                    Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    pickImage.launch(intent);
+            });
+
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
@@ -64,6 +93,22 @@ public class Add_food extends AppCompatActivity {
         LayoutInflater inflater = LayoutInflater.from(this);
         View ingredientRow = inflater.inflate(R.layout.ingredient_rows, ingredientContainer, false);
         ingredientContainer.addView(ingredientRow);
+    }
+
+    private  File saveBitmapToFile(Bitmap bitmap) {
+        File fileDir = getFilesDir();
+        File imageFile = new File(fileDir, "dish.jpeg");
+        try {
+            FileOutputStream fos = new FileOutputStream(imageFile);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+            fos.flush();
+            fos.close();
+            return imageFile;
+        } catch (Exception e) {
+            Log.d("IMAGE_ADD_FOOD", e.getMessage());
+            Toast.makeText(this, "Error saving image: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            return null;
+        }
     }
 
 
